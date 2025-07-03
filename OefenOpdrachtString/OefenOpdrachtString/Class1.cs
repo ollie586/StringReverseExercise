8e8e52826d0e64da6d9f9a08fab15704180fc08c
ollie586 <153005025+ollie586@users.noreply.github.com>
2025-07-03 13:32:59 +0200

    Opdracht added

---
+++ OefenOpdrachtString/OefenOpdrachtString/Class1.cs
@@ -0,0 +1,155 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace OefenOpdrachtString;
+
+public class StringReverser
+{
+    public string Opdracht1(string text)
+    {
+        // '' = character "" = string in c#
+        var arr = text.Split(' ');
+        var b = arr.Reverse();
+        var result = string.Join(' ', b);
+        return result;
+    }
+
+    public string Opdracht2(string text)
+    {
+        var arr = text.Split(' ');
+        var result = "";
+        foreach (var b in arr)
+        {
+            var letters = b.ToCharArray().Reverse();
+            foreach (var c in letters)
+            {
+                result += c;
+            }
+
+            if (arr[13] != b)
+            {
+                result += " ";
+            }
+        }
+
+        return result;
+
+    }
+    // Reverse casing:
+    public string Opdracht3_1(string text)
+    {
+        var arr = text.Split(" ");
+        var result = "";
+        foreach (var b in arr)
+        {
+            var letters = b.ToCharArray();
+            foreach (var c in letters)
+            {
+                if (char.IsUpper(c))
+                {
+                    result += char.ToLower(c);
+                }
+                else if (char.IsLower(c))
+                {
+                    result += char.ToUpper(c);
+                }
+                else
+                {
+                    result += c;
+                }
+
+            }
+
+            if (arr[13] != b)
+            {
+                result += " ";
+            }
+        }
+        return result;
+    }
+
+    // Capitalize each word:
+    public string Opdracht3_2(string text)
+    {
+        var arr = text.Split(" ");
+        var result = "";
+        foreach (var b in arr)
+        {
+            var letters = b.ToCharArray();
+            var count = 1;
+            foreach (var c in letters)
+            {
+                if (c == '.' || c == ':' || c == ',' || c == '!' || c == '?')
+                {
+                    result += c;
+                }
+                else
+                {
+                    if (count == 1)
+                    {
+                        result += char.ToUpper(c);
+                        count++;
+                    }
+                    else
+                    {
+                        result += char.ToLower(c);
+                        count++;
+                    }
+                }
+            }
+
+            if (arr[13] != b)
+            {
+                result += " ";
+            }
+        }
+        return result;
+    }
+
+    //Capitalize first letter of each sentence:
+    public string Opdracht3_3(string text)
+    {
+        var arr = text.Split(" ");
+        var result = "";
+        var count = 1;
+        foreach (var b in arr)
+        {
+            var letters = b.ToCharArray();
+
+            foreach (var c in letters)
+            {
+                if (c == '.' || c == ':' || c == ',' || c == '!' || c == '?')
+                {
+                    result += c;
+                    if (c == '.' || c == '!' || c == '?')
+                    {
+                        count = 1;
+                        var characters = result.Count();
+                        var remaining = characters - 2;
+                        result = result.Remove(remaining, 2);
+                        result += c + " ";
+                    }
+                }
+                else
+                {
+                    if (count == 1)
+                    {
+                        result += char.ToUpper(c);
+                        count++;
+                    }
+                    else
+                    {
+                        result += char.ToLower(c);
+
+                    }
+                }
+
+            }
+
+            if (arr[13] != b)
+            {
+                result += " ";
+            }
+        }
+        return result;
+    }
+}
---
+++ OefenOpdrachtString/OefenOpdrachtStringTests/UnitTest1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using OefenOpdrachtString;
+
+namespace OefenOpdrachtStringTests;
+
+public class UnitTest1
+{
+//    Input A:  ""
+//Output B: 
+    [Fact]
+    public void Opdracht1_WithText_ReturnsReversedText()
+    {
+        //arrange
+        string s = "Hello developer. Do you like any of these fruits: apple, pear, banana? I do!";
+        string result = "do! I banana? pear, apple, fruits: these of any like you Do developer. Hello";
+
+        //act
+        var c = new StringReverser();
+        string r = c.Opdracht1(s);
+
+        //assert
+        Assert.Equal(result, r);
+    }
+
+    [Fact]
+    public void Opdracht2_WithText_ReturnsReversedWords()
+    {
+        //arrange
+        string s = "Hello developer. Do you like any of these fruits: apple, pear, banana? I do!";
+        string result = "olleH .repoleved oD uoy ekil yna fo eseht :stiurf ,elppa ,raep ?ananab I !od";
+
+        //act
+        var c = new StringReverser();
+        string r = c.Opdracht2(s);
+
+        //assert
+        Assert.Equal(result, r);
+    }
+
+    // Reverse casing:
+
+    // Capitalize each word:
+
+    // Capitalize first letter of each sentence:
+
+    [Fact]
+    public void Opdracht3_1_WithText_ReturnsReversedCasing()
+    {
+        //arrange
+        string s = "olleH .repoleved oD uoy ekil yna fo eseht :stiurf ,elppa ,raep ?ananab I !od";
+        string result = "OLLEh .REPOLEVED Od UOY EKIL YNA FO ESEHT :STIURF ,ELPPA ,RAEP ?ANANAB i !OD";
+
+        //act
+        var c = new StringReverser();
+        string r = c.Opdracht3_1(s);
+
+        //assert
+        Assert.Equal(result, r);
+    }
+
+    [Fact]
+    public void Opdracht3_2_WithText_ReturnsReversedEachWordCapitalized()
+    {
+        //arrange
+        string s = "olleH .repoleved oD uoy ekil yna fo eseht :stiurf ,elppa ,raep ?ananab I !od";
+        string result = "Olleh .Repoleved Od Uoy Ekil Yna Fo Eseht :Stiurf ,Elppa ,Raep ?Ananab I !Od";
+
+        //act
+        var c = new StringReverser();
+        string r = c.Opdracht3_2(s);
+
+        //assert
+        Assert.Equal(result, r);
+    }
+
+    [Fact]
+    public void Opdracht3_3_WithText_ReturnsReversedEachFirstWordCapitalized()
+    {
+        //arrange
+        string s = "olleH .repoleved oD uoy ekil yna fo eseht :stiurf ,elppa ,raep ?ananab I !od";
+        string result = "Olleh. Repoleved od uoy ekil yna fo eseht :stiurf ,elppa ,raep? Ananab i! Od";
+
+        //act
+        var c = new StringReverser();
+        string r = c.Opdracht3_3(s);
+
+        //assert
+        Assert.Equal(result, r);
+    }
+
+    
+}
